86425ea54d3ad23bd6549155dbc921ae0554118e
unknown <indi1337.pi@gmail.com>
2026-04-13 16:13:04 +0200

    portfix

--- api.py
+++ api.py
@@ -49,4 +49,4 @@ def get_collection(collection_name: str):
 
 if __name__ == "__main__":                                  # 
     import uvicorn
-    uvicorn.run("api:app", host="127.0.0.1", port=8008)
+    uvicorn.run("api:app", host="0.0.0.0", port=8008)
