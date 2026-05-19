008bb13d181f2e73bee7c191afd156358305f176
ottokopp <kopp_otto@yahoo.de>
2026-05-19 13:38:58 +0200

    update name key to username in add_user endpoint

--- api.py
+++ api.py
@@ -6,21 +6,30 @@ db = client["testdb"]
 collection = db["test"]     
 app = FastAPI()                      
 
-for doc in collection.find():                               # Alle Dokumente abrufen
-    print(doc)
-
 @app.get("/health")                                         # Root-Endpoint
 def root():
     return {"message": "API läuft"}
 
-@app.get("/add_user")                                       # Benutzer hinzufügen
-def add_user(name: str):
-    result = db["users"].insert_one({"name": name})
-    return {"id": str(result.inserted_id), "name": name}
+name = "Indi"
+
+dictionary = {
+    "name": "Indi",
+    "alter": 12,
+    "hobbies": ["goonen", "golfen", "coden"]
+}
+
+@app.post("/add_user")                                       # Benutzer hinzufügen
+def add_user(user_dict: dict):
+    
+    if db["users"].find_one({"username": user_dict["username"]}):
+        return {"error": "User already exists"}
+    
+    result = db["users"].insert_one(user_dict)
+    return {"id": str(result.inserted_id), "username": user_dict["username"]}
 
 @app.get("/get_user")                                       # Benutzer abrufen
-def get_user(name: str):
-    user = db["users"].find_one({"name": name})
+def get_user(username: str):
+    user = db["users"].find_one({"username": username})
     if user:
         user["_id"] = str(user["_id"])
         return user
